a6efb96b17925c17c63d2dbabceda65831a61550
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2024-05-14 11:19:45 -0300

    nivel-03-modulo-01

--- nivel-03-minha-primeira-api-em-dotnet/MyFirstApi/Controllers/UserController.cs
+++ nivel-03-minha-primeira-api-em-dotnet/MyFirstApi/Controllers/UserController.cs
@@ -7,5 +7,12 @@ namespace MyFirstApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get([FromRoute] int id, [FromRoute] string nickName, [FromHeader] int? age)
+        {
+            return Ok("Reinan");
+        }
     }
 }
--- nivel-03-minha-primeira-api-em-dotnet/MyFirstApi/Program.cs
+++ nivel-03-minha-primeira-api-em-dotnet/MyFirstApi/Program.cs
@@ -1,18 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var teste = builder.Configuration.GetSection("Prop2").Value;
 var teste2 = builder.Configuration.GetValue<string>("Object:Prop1");
 
+//obriga as rotas ficarem minusculas
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
