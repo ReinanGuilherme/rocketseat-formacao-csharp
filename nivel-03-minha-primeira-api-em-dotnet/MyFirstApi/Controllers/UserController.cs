950a4659d8357fe3a190703d1cf3b1d44d0ccb5e
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2024-05-14 15:25:10 -0300

    nivel-03-modulo-02

--- nivel-03-minha-primeira-api-em-dotnet/MyFirstApi/Controllers/UserController.cs
+++ nivel-03-minha-primeira-api-em-dotnet/MyFirstApi/Controllers/UserController.cs
@@ -49,5 +49,25 @@ namespace MyFirstApi.Controllers
         {
             return NoContent();
         }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<User>),StatusCodes.Status204NoContent)]
+        public IActionResult GetAll()
+        {
+            var response = new List<User>()
+            {
+                new User() {Id= 1, Age = 7, Name="Reinan"},
+                new User() {Id= 2, Age = 7, Name="Maria"}
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPut("change-password")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult ChangePassword([FromBody] RequestChangePasswordJson request)
+        {
+            return NoContent();
+        }
     }
 }
