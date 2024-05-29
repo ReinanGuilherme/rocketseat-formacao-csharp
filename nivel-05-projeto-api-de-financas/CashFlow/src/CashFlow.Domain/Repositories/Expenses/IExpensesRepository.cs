6ac80478b58851d80131391a5798acefea7e7032
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2024-05-29 11:41:32 -0300

    nivel-05-2.03

---
+++ nivel-05-projeto-api-de-financas/CashFlow/src/CashFlow.Domain/Repositories/Expenses/IExpensesRepository.cs
@@ -0,0 +1,10 @@
+﻿using CashFlow.Domain.Entities;
+
+namespace CashFlow.Domain.Repositories.Expenses
+{
+    public interface IExpensesRepository
+    {
+
+        void Add(Expense expense);
+    }
+}
