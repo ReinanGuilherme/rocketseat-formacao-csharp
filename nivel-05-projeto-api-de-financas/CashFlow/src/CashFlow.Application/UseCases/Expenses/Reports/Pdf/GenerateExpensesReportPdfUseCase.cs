519db6b3afadbcdbd0a446b5d5ba96595fabade5
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2024-06-25 14:21:49 -0300

    atualizando

--- nivel-05-projeto-api-de-financas/CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ nivel-05-projeto-api-de-financas/CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,4 +1,7 @@
-﻿using CashFlow.Domain.Repositories.Expenses;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
+using PdfSharp.Fonts;
 using System.Globalization;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
@@ -11,22 +14,39 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
         public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
         {
             _repository = repository;
+
+            GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
         }
 
         public async Task<byte[]> Execute(string month)
         {
 
-            DateTime monthFormatter = DateTime.ParseExact(month, "yyyy-MM", CultureInfo.InvariantCulture);
+            DateTime monthFormatted = DateTime.ParseExact(month, "yyyy-MM", CultureInfo.InvariantCulture);
 
-            var expenses = await _repository.FilterByMonth(monthFormatter);
+            var expenses = await _repository.FilterByMonth(monthFormatted);
             if (expenses.Count == 0)
             {
                 return [];
             }
 
+            var document = CreateDocument("Reinan",monthFormatted);
+
 
 
             return [];
+        }
+
+        private Document CreateDocument(string author, DateTime month)
+        {
+            var document = new Document();
+
+            document.Info.Title = "Titulo documento" + month.ToString("Y");
+            document.Info.Author = author;
+
+            var style = document.Styles["Normal"];
+            style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+            return document;
         }
     }
 }
